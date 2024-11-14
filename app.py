c018cc0a983d3a8ab3bbdb65126692c43f4851d4
Leonardo Nogueira <112121794+lnwrdo@users.noreply.github.com>
2024-11-14 22:11:45 +0000

    aaa

--- app.py
+++ app.py
@@ -24,11 +24,6 @@ def escolha_filme():
     genres = df['Genre'].unique()  # Obtenha os gêneros únicos
     return render_template('escolha.html', genres=genres)
 
-@app.route('/sobre')
-def sobre_o_projeto():
-    genres = df['Genre'].unique()  # Obtenha os gêneros únicos
-    return render_template('sobre.html', genres=genres)
-
 @app.route('/recommend', methods=['POST'])
 def recommend():
     selected_genre = request.form['genre']
